6048e267d969d931f893960693a6a5fb9869836a
Andrew LeGrand <111323440+Andrew-LeGrand@users.noreply.github.com>
2023-04-27 18:10:21 -0500

    Book component and book model start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AuthComponent } from './shared/auth/auth.component';
 import { NavigationComponent } from './shared/navigation/navigation.component';
 import { LibraryComponent } from './library/library.component';
 import { AuthInterceptorService } from './shared/auth/auth-interceptor.service';
+import { BookComponent } from './shared/book/book.component';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent, NavigationComponent, LibraryComponent],
+  declarations: [AppComponent, AuthComponent, NavigationComponent, LibraryComponent, BookComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [
     {
---
+++ src/app/shared/book/book.model.ts
@@ -0,0 +1,14 @@
+export class Book {
+    constructor(
+        public title: string,
+        public author: string,
+        public coverImagePath: string,
+        public length: string,
+        public notes?: string,
+        public genre?: string,
+    ) {}
+}
+
+// work on book component layout
+// work on library component layout
+// work on creating a new book (rails, then postman, then angular)
